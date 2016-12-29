b5a003a5aa9141c514c6f50af39cc7721b3926b3
Sam Kim <samkim28@gmail.com>
2016-12-29 14:48:57 -0800

    [setup] creates basic file structure with files

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const router = require('./router');
+require('dotenv').config()
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+app.use(express.static('client'));
+app.use('/', router);
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname + '/client/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
