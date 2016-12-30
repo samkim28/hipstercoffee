e0429afcbc96b8e4dd563d07e7826f12bcee23c3
Sam Kim <samkim28@gmail.com>
2016-12-29 16:17:42 -0800

    [feature] adds search endpoint that returns list of coffee shops

---
+++ client/actions/coffee.js
@@ -0,0 +1,28 @@
+import { FETCH_COFFEE } from './types';
+import axios from 'axios';
+
+export function fetchCoffee(input) {
+  const data = {
+    params: {
+      input: input
+    }
+  };
+  return axios.get('/search', data)
+    .then(checkStatus)
+    .then((response) => {
+      return { type: FETCH_COFFEE, payload: response.data }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function checkStatus(response) {
+  if(response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
--- server/router.js
+++ server/router.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const request = require('request');
 const router = express.Router();
+const Yelp = require('yelp');
 require('dotenv').config();
 
-// router.get('/random', (req, res) => {
-//   const url = 'https://api.giphy.com/v1/gifs/random';
-//   const qs = {
-//     api_key: api_key,
-//     tag: 'cat'
-//   }
-//
-//   request.get({url, qs}, (error, response, body) => {
-//     body = JSON.parse(body);
-//     if (error) {
-//         res.status(400).send(error);
-//     } else {
-//         res.status(200).json(body);
-//     }
-//   });
-// });
+const yelp = new Yelp({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.TOKEN,
+  token_secret: process.env.TOKEN_SECRET
+});
+
+router.get('/search', (req, res) => {
+  yelp.search({
+    term: 'hipster coffee',
+    radius_filter: '20000',
+    limit: '5',
+    location: req.query.input
+  })
+  .then((data) => {
+    console.log('data: ', data);
+    res.status(200).json(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+})
+
 //
 // router.get('/search', (req, res) => {
 //   const url = 'https://api.giphy.com/v1/gifs/search';
